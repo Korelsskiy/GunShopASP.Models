0a5c240087158719c206b8136782cc258fa1463e
Korelsskiy <68390165+Korelsskiy@users.noreply.github.com>
2020-12-04 23:41:30 +0300

    Добавьте файлы проекта.

---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GunShopASP.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Producer { get; set; }
+        public string Country { get; set; }
+        public decimal Price { get; set; }
+        public string PhotoPath { get; set; }
+        public Cat? Category{ get; set; }
+    }
+}
